ed901999adefe7fbd6a1ec9b576491e146436c57
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-10 20:03:06 +0530

    commit

---
+++ PalindromicPyramid.java
@@ -0,0 +1,22 @@
+public class PalindromicPyramid {
+    public static void main(String args[]){
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print the first half of the row
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            // Print the second half of the row
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
