a1d69621925a489c7feecf0b02386372e6046753
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-11 01:13:13 +0530

    commit

--- Pattern/tempCodeRunnerFile.java
+++
@@ -1,7 +0,0 @@
-package Pattern;
-for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i+1;j++){
-                System.out.print(j+" ");
-            }
-            System.out.println();
-        }
