83b083eeb2c44ddb2e86122e7c5eab198036ac18
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-11 00:20:55 +0530

    commit

---
+++ HollowButterfly.java
@@ -0,0 +1,51 @@
+public class HollowButterfly {
+    public static void main(String args[]){
+        int n=4;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                {
+                    if(j==1 || j==i )
+                        System.out.print("*"+" ");
+                    else
+                        System.out.print(" "+" ");
+                    }
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" "+" ");
+            }
+            for(int j=1;j<=i;j++){
+                {
+                    if(j==1 || j==i )
+                    System.out.print("*"+" ");
+                else
+                    System.out.print(" "+" ");
+                }
+        }
+            System.out.println();
+        }
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                {
+                    if(j==1 || j==i )
+                        System.out.print("*"+" ");
+                    else
+                        System.out.print(" "+" ");
+                    }
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" "+" ");
+            }
+            for(int j=1;j<=i;j++){
+                {
+                    if(j==1 || j==i )
+                        System.out.print("*"+" ");
+                    else
+                        System.out.print(" "+" ");
+                    }
+            }
+            System.out.println();
+        }
+    }
+    }
