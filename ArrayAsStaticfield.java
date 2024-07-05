b1cf26112385bf988339f26ec57be039787fbcfb
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-07-05 18:48:12 +0530

    commit

---
+++ ArrayAsStaticfield.java
@@ -0,0 +1,13 @@
+public class ArrayAsStaticfield {
+    private static int[] staticArray={1,2,3,4,5};
+
+    public static void printStaticArray(){
+        for (int i:staticArray){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]){
+        ArrayAsStaticfield.printStaticArray();
+    }
+}
