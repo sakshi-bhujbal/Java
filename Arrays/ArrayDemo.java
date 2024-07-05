2bb67995eef912374dddb01dd8c88340afd36dd0
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-07-06 00:57:12 +0530

    commit

---
+++ Arrays/ArrayDemo.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class ArrayDemo {
+    public static void main(String args[]){
+        int [] marks=new int[3];
+
+        marks[0]=1;
+        marks[1]=8;
+        marks[2]=9;
+        System.out.println(marks);
+    }
+}
