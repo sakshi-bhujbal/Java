36cfa5eec411e0f42e430226095d1433940a2b06
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-07-06 00:35:47 +0530

    commit

---
+++ Arrays/Arrays.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class Arrays {
+    
+    /**
+     * @param args
+     */
+    public static void main(String args[]){
+    int [] marks= new int[3];
+    marks[0]=45;
+    marks[1]=56;
+    marks[2]=89;
+    System.out.println(marks);
+    }
+}
