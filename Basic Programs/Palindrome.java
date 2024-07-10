a7f9a3e5a71f771d5d9ce3ae4db188b45686f549
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-07-10 23:36:58 +0530

    commit

---
+++ Basic Programs/Palindrome.java
@@ -0,0 +1,19 @@
+
+public class Palindrome {
+   public static void main(String args[]){
+    int number=151;
+    int temp=number;
+    int sum=0;
+    while(number>0){
+        int rem=number%10;
+        sum=sum*10 +rem;
+        number=number/10;
+    }
+    if(temp==sum){
+        System.out.println("The number is palindrome number");
+    }
+    else{
+        System.out.println("Not a palindrome number");
+    }
+   } 
+}
