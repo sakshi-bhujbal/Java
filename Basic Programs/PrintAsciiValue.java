ffa32075cb3f1313884859829028037cad7f3543
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-07-14 18:29:45 +0530

    commit

---
+++ Basic Programs/PrintAsciiValue.java
@@ -0,0 +1,8 @@
+public class PrintAsciiValue {
+    public static void main(String args[]){
+        int ch1='a';
+        System.out.println("The ascii value of a is: "+ch1);
+        int ch=(int)ch1;
+        System.out.println(ch1);
+    }
+}
---
+++ Basic Programs/RandomNumber.java
@@ -0,0 +1,7 @@
+import java.lang.Math;
+public class RandomNumber {
+    public static void main(String args[]){
+        System.out.println("First random number is "+Math.random());
+        System.out.println("Second random number is "+Math.random());
+    }
+}
