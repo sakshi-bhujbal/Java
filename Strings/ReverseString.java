3e01de8ae5b539e6b7ce087bf71cb5845a6f95d2
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-07-23 22:42:18 +0530

    commit

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -2,7 +2,7 @@ package Strings;
 
 public class ReverseString {
     public static void main(String args[]){
-        String str="Sakshi";
+        String str="Sakshi Bhujbal";
         char ch[]=str.toCharArray();
         String str1=" ";
         for(int i=ch.length-1;i>=0;i--) {
