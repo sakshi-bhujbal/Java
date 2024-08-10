8f1c92c792e1843d044b76865fa0426db2e60038
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-08-10 23:59:23 +0530

    commit

--- CollectionFramework/linkedlistdemo.java
+++ CollectionFramework/linkedlistdemo.java
@@ -1,5 +1,32 @@
 package CollectionFramework;
+import java.util.*;
 
 public class linkedlistdemo {
+    public static void main(String args[]){
+   LinkedList<String> list=new LinkedList<String>();
+   
+   list.addFirst("a");
+   list.addFirst("is");
+   System.out.println(list);
+   list.addFirst("this");
+   System.out.println(list);
+
+   list.addLast("list");
+   //System.out.println(list);
+
+    System.out.println(list.size());
+
+    // for(int i=0;i<list.size();i++){
+    //     System.out.print(list.get(i)+" -> ");
+    // }
+
+    // for(String i:list){
+    //     System.out.print(i+" -> ");
+    // }
     
+    list.removeFirst();
+    System.out.println(list);
+    list.removeLast();
+    System.out.println(list);
+}
 }
