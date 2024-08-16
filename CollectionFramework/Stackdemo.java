a6ca1c8ca1ff588affc06296290b0dd162ab68e6
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-08-16 19:55:48 +0530

    commit

--- CollectionFramework/Stackdemo.java
+++ CollectionFramework/Stackdemo.java
@@ -1,45 +1,46 @@
+import java.util.*;
 public class Stackdemo {
-    static class Node{
-        int data;
-        Node next;
+    // static class Node{
+    //     int data;
+    //     Node next;
 
-       public  Node(int data){
-            this.data=data;
-            next=null;
-        }
-    }
-        static class Stack{
-            public static Node head;
-            public static boolean isEmpty(){
-                return head == null;
-            }
-            public static void push(int data){
-                Node newNode=new Node(data);
-                if(head==null){
-                    head=newNode;
-                    return;
-                }
-                newNode.next=head;
-                head=newNode;
-            }
+    //    public  Node(int data){
+    //         this.data=data;
+    //         next=null;
+    //     }
+    // }
+    //     static class Stack{
+    //         public static Node head;
+    //         public static boolean isEmpty(){
+    //             return head == null;
+    //         }
+    //         public static void push(int data){
+    //             Node newNode=new Node(data);
+    //             if(head==null){
+    //                 head=newNode;
+    //                 return;
+    //             }
+    //             newNode.next=head;
+    //             head=newNode;
+    //         }
         
-        public static int pop(){
-            if(isEmpty()){
-                return -1;
-            }
-            int top=head.data;
-            head=head.next;
-            return top;
-        } 
-        public static int peek(){
-            if(isEmpty()){
-                return -1;
-            }
-            return head.data;
-        }
-    }
+    //     public static int pop(){
+    //         if(isEmpty()){
+    //             return -1;
+    //         }
+    //         int top=head.data;
+    //         head=head.next;
+    //         return top;
+    //     } 
+    //     public static int peek(){
+    //         if(isEmpty()){
+    //             return -1;
+    //         }
+    //         return head.data;
+    //     }
+    // }
     public static void main(String args[]){
-        Stack s=new Stack();
+        Stack <Integer> s=new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
